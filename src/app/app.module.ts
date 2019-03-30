90bfa89de82b67f0a288b65ff97bbeea9d190820
sanj52 <shtsanjay52@gmail.com>
2019-03-30 20:14:00 +0545

    user component managed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ProductComponent } from './product/product.component';
-import { DemoComponent } from './demo/demo.component';
-import { ModulesComponent } from './demo/modules/modules.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AddUserComponent } from './user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     ProductComponent,
-    DemoComponent,
-    ModulesComponent
+    AddUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+import { Router } from '@angular/router';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+
+  constructor(private router: Router, private userService: UserService) {
+
+  }
 
   ngOnInit() {
-  }
+    this.userService.getUsers()
+      .subscribe( data => {
+        this.users = data;
+      });
+  };
+
+  deleteUser(user: User): void {
+    this.userService.deleteUser(user)
+      .subscribe( data => {
+        this.users = this.users.filter(u => u !== user);
+      })
+  };
+
 
 }
